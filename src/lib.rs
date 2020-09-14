b54e231e06aa4aa35520b98a2a23b071e19a625e
João M. Bezerra <marcospb19@hotmail.com>
2020-09-13 22:47:59 -0300

    Merging src/unix.rs and src/windows.rs
    Now using conditional compilation correctly to have the cross-platform
    lib in one file.
    
    Also changed the API.

--- src/lib.rs
+++ src/lib.rs
@@ -76,7 +76,7 @@
 //!     let path = ".git";
 //!     let file_type = FileType::try_from_path(path).unwrap();
 //!
-//!     if !file_type.is_dir() {
+//!     if !file_type.is_directory() {
 //!         println!("We are not inside of a git repository.");
 //!     }
 //! }
@@ -101,12 +101,169 @@
 //! }
 //! ```
 
-#[cfg(unix)]
-mod unix;
-#[cfg(unix)]
-pub use unix::FileType;
+use std::{fmt, fs, io, path::Path};
 
-#[cfg(windows)]
-mod windows;
-#[cfg(windows)]
-pub use windows::FileType;
+#[cfg(unix)]
+use std::os::unix::fs::FileTypeExt;
+
+/// # Variants:
+///
+/// ```ignore
+/// match file_type {
+///     FileType::File        => { /* ... */ },
+///     FileType::Directory   => { /* ... */ },
+///     FileType::Symlink     => { /* ... */ },
+///     FileType::BlockDevice => { /* ... */ },
+///     FileType::CharDevice  => { /* ... */ },
+///     FileType::Fifo        => { /* ... */ },
+///     FileType::Socket      => { /* ... */ },
+/// }
+/// ```
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy, Ord, PartialOrd)]
+pub enum FileType {
+    File,
+    Directory,
+    Symlink,
+    #[cfg(unix)]
+    BlockDevice,
+    #[cfg(unix)]
+    CharDevice,
+    #[cfg(unix)]
+    Fifo,
+    #[cfg(unix)]
+    Socket,
+}
+
+impl FileType {
+    /// Try to get `FileType` from a path.
+    ///
+    /// This function follows symlinks, so it can never return a
+    /// FileType::Symlink.
+    pub fn from_path(path: &dyn AsRef<Path>) -> Result<Self, io::Error> {
+        let fs_file_type = fs::metadata(path.as_ref())?.file_type();
+        let result = FileType::from(fs_file_type);
+        Ok(result)
+    }
+
+    /// Try to get `FileType` from a path.
+    ///
+    /// Don't follow symlinks, so the result can be the variant
+    /// `FileType::Symlink` too.
+    pub fn from_symlink_path(path: &dyn AsRef<Path>) -> Result<Self, io::Error> {
+        let fs_file_type = fs::symlink_metadata(path.as_ref())?.file_type();
+        let result = FileType::from(fs_file_type);
+        Ok(result)
+    }
+
+    pub fn is_file(&self) -> bool {
+        match self {
+            FileType::File => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_directory(&self) -> bool {
+        match self {
+            FileType::Directory => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_symlink(&self) -> bool {
+        match self {
+            FileType::Symlink => true,
+            _ => false,
+        }
+    }
+
+    #[cfg(unix)]
+    pub fn is_block_device(&self) -> bool {
+        match self {
+            FileType::BlockDevice => true,
+            _ => false,
+        }
+    }
+
+    #[cfg(unix)]
+    pub fn is_char_device(&self) -> bool {
+        match self {
+            FileType::CharDevice => true,
+            _ => false,
+        }
+    }
+
+    #[cfg(unix)]
+    pub fn is_fifo(&self) -> bool {
+        match self {
+            FileType::Fifo => true,
+            _ => false,
+        }
+    }
+
+    #[cfg(unix)]
+    pub fn is_socket(&self) -> bool {
+        match self {
+            FileType::Socket => true,
+            _ => false,
+        }
+    }
+}
+
+impl From<fs::FileType> for FileType {
+    fn from(ft: fs::FileType) -> Self {
+        // Check each type, except for symlink, because fs::metadata() follows symlinks
+        #[cfg(unix)]
+        let result = {
+            if ft.is_file() {
+                FileType::File
+            } else if ft.is_dir() {
+                FileType::Directory
+            } else if ft.is_symlink() {
+                FileType::Symlink
+            } else if ft.is_block_device() {
+                FileType::BlockDevice
+            } else if ft.is_char_device() {
+                FileType::CharDevice
+            } else if ft.is_fifo() {
+                FileType::Fifo
+            } else if ft.is_socket() {
+                FileType::Socket
+            } else {
+                panic!();
+            }
+        };
+
+        #[cfg(windows)]
+        let result = {
+            if ft.is_file() {
+                FileType::File
+            } else if ft.is_dir() {
+                FileType::Directory
+            } else if ft.is_symlink() {
+                FileType::Symlink
+            } else {
+                panic!();
+            }
+        };
+
+        result
+    }
+}
+
+impl fmt::Display for FileType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            FileType::File => write!(f, "regular file"),
+            FileType::Directory => write!(f, "directory"),
+            FileType::Symlink => write!(f, "symbolic link"),
+            #[cfg(unix)]
+            FileType::BlockDevice => write!(f, "block device"),
+            #[cfg(unix)]
+            FileType::CharDevice => write!(f, "char device"),
+            #[cfg(unix)]
+            FileType::Fifo => write!(f, "FIFO"),
+            #[cfg(unix)]
+            FileType::Socket => write!(f, "socket"),
+        }
+    }
+}
